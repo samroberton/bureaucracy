41435050192bb6319f8d55d45b3382a6afa5f417
Sam Roberton <sam@samroberton.com>
2015-11-08 22:57:12 +1100

    .dir-locals.el fix for s/defrecord

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((clojure-mode
-  (eval put-clojure-indent 's/defrecord 2)
   (eval put-clojure-indent 'this-as 1)
   (eval put 's/defrecord 'clojure-backtracking-indent '(4 4 (2)))))

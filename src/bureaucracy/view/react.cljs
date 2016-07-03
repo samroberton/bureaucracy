8eebd142b84ce106704fc69c5e57cd7fc987337a
Sam Roberton <sam@samroberton.com>
2016-07-03 16:09:27 +1000

    Fix use of this-as

--- src/bureaucracy/view/react.cljs
+++ src/bureaucracy/view/react.cljs
@@ -1,27 +1,27 @@
 (ns bureaucracy.view.react
   (:require [bureaucracy.view :as view]))
 
-(defn- should-component-update [next-props _]
-  (this-as this
-    (or (not= (aget (.-props this) "app_db")
-              (aget next-props "app_db"))
-        (not= (aget (.-props this) "state_dbs")
-              (aget next-props "state_dbs"))
-        (not= (aget (.-props this) "subviews")
-              (aget next-props "subviews")))))
+(defn- should-component-update [next-props props]
+  (or (not= (aget props "app_db")
+            (aget next-props "app_db"))
+      (not= (aget props "state_dbs")
+            (aget next-props "state_dbs"))
+      (not= (aget props "subviews")
+            (aget next-props "subviews"))))
 
-(defn- renderer [view-fn view-name]
-  (fn []
-    (this-as this
-      (let [props (.-props this)]
-        (view-fn (aget props "callbacks") (aget props "app_db") (aget props "state_dbs"))))))
+(defn- render [view-fn view-name props]
+  (view-fn (aget props "callbacks") (aget props "app_db") (aget props "state_dbs")))
 
 (defn- node->react-factory [react {:keys [view-name view-fn] :as node}]
   ((.-createFactory react)
    ((.-createClass react)
     #js {:displayName           view-name
-         :shouldComponentUpdate should-component-update
-         :render                (renderer view-fn view-name)})))
+         :shouldComponentUpdate (fn [next-props _]
+                                  (this-as this
+                                    (should-component-update next-props (.-props this))))
+         :render                (fn []
+                                  (this-as this
+                                    (render view-fn view-name (.-props this))))})))
 
 (defn wrap-node [react node]
   (let [react-factory (node->react-factory react node)]
